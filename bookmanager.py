35b6b626777ed947bc5da0d5084e3082027fa0d0
amandadr <amandadroy@gmail.com>
2024-02-02 09:54:17 -0400

    set up create

--- bookmanager.py
+++ bookmanager.py
@@ -1,10 +1,30 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+# Set up the Flask app
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 
-@app.route("/")
+db = SQLAlchemy(app)
+
+# Define the Book model
+class Book(db.Model):
+    title = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+
+    def __repr__(self):
+        return "<Title: {}>".format(self.title)
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "My flask app"
+    if request.form:
+        book = Book(title=request.form.get("title"))
+        db.session.add(book)
+        db.session.commit()
+    return render_template("home.html")
   
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
