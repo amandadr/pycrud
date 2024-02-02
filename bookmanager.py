993fa88f01da3bead19cdb497588979182121e98
amandadr <amandadroy@gmail.com>
2024-02-02 10:20:33 -0400

    set up RUD functionality

--- bookmanager.py
+++ bookmanager.py
@@ -11,20 +11,50 @@ app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 
 db = SQLAlchemy(app)
 
+app.app_context().push()
+
 # Define the Book model
 class Book(db.Model):
     title = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
 
     def __repr__(self):
         return "<Title: {}>".format(self.title)
-
+    
+# Define the routes
 @app.route("/", methods=["GET", "POST"])
 def home():
+    books = None
     if request.form:
-        book = Book(title=request.form.get("title"))
-        db.session.add(book)
+        try:
+            book = Book(title=request.form.get("title"))
+            db.session.add(book)
+            db.session.commit()
+        except Exception as e:
+            print("Failed to add book")
+            print(e)
+    books = Book.query.all()
+    return render_template("home.html", books=books)
+
+@app.route("/update", methods=["POST"])
+def update():
+    try:
+        newtitle = request.form.get("newtitle")
+        oldtitle = request.form.get("oldtitle")
+        book = Book.query.filter_by(title=oldtitle).first()
+        book.title = newtitle
         db.session.commit()
-    return render_template("home.html")
+    except Exception as e:
+        print("Couldn't update book title")
+        print(e)
+    return redirect("/")
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    title = request.form.get("title")
+    book = Book.query.filter_by(title=title).first()
+    db.session.delete(book)
+    db.session.commit()
+    return redirect("/")
   
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
